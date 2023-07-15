184c3c11d2bac8dc0e2d8c6187a5c5e69f835300
Hiro <laciferin@gmail.com>
2023-07-16 05:28:48 +0530

    add: config service
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,43 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLDateTime } from 'graphql-iso-date'
+import { ConfigModule } from '@nestjs/config'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground'
 import { DonationsModule } from './donations/donations.module'
 import { PrismaModule } from './prisma/prisma.module'
+import * as Joi from 'joi'
+
+let mode = process.env.MODE
+let envFile = '.env'
+
+switch (mode) {
+  case 'test':
+    envFile = '.env.test'
+    process.env.NODE_ENV = 'testing'
+    break
+  case 'prod':
+    process.env.NODE_ENV = 'production'
+    envFile = '.env.prod'
+    break
+  default:
+    mode = 'dev'
+    process.env.NODE_ENV = 'development'
+    envFile = '.env.local'
+}
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [envFile, '.env'],
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        MODE: Joi.string().valid('dev', 'prod', 'test').default('dev'),
+        PORT: Joi.number().default(1606),
+        DATABASE_URL: Joi.string(),
+      }),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       playground: false,
       driver: ApolloDriver,
