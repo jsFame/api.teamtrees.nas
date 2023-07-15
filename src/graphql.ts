939ba8cff3c93c6bb352a3633fc754494ddbaaaf
Hiro <laciferin@gmail.com>
2023-07-16 05:21:02 +0530

    remove: imports
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -5,7 +6,6 @@
  */
 
 /* tslint:disable */
-
 /* eslint-disable */
 
 export class CreateDonationInput {
@@ -13,19 +13,17 @@ export class CreateDonationInput {
 }
 
 export class Donation {
-  exampleField?: Nullable<number>
-}
+  exampleField?: Nullable<number>;
+
 
 export abstract class IQuery {
-  abstract donations(): Nullable<Donation>[] | Promise<Nullable<Donation>[]>
+  abstract donations(): Nullable<Donation>[] | Promise<Nullable<Donation>[]>;
 
-  abstract donation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>
+  abstract donation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>;
 }
 
 export abstract class IMutation {
-  abstract createDonation(
-    createDonationInput: CreateDonationInput,
-  ): Donation | Promise<Donation>
+  abstract createDonation(createDonationInput: CreateDonationInput): Donation | Promise<Donation>;
 }
 
-type Nullable<T> = T | null
+type Nullable<T> = T | null;
