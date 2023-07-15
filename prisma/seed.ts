e6f9a0e3034256effda99515375fafde7031f98f
Hiro <laciferin@gmail.com>
2023-07-16 04:34:24 +0530

    style: prettier
    Took 5 minutes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,24 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.donation.deleteMany();
+  await prisma.donation.deleteMany()
   const hiro = await prisma.donation.create({
     data: {
       email: 'hiroe@teamtrees.nas',
       displayName: 'hiro',
       count: 5,
     },
-  });
+  })
 
-  console.log({ hiro });
+  console.log({ hiro })
 }
 
 main()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { GraphQLDateTime } from 'graphql-iso-date';
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { GraphQLDateTime } from 'graphql-iso-date'
 
 @Module({
   imports: [
