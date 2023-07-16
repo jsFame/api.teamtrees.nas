c25ec1bdd2d3d7b61e3e18753c4cea8e9257e122
Hiro <laciferin@gmail.com>
2023-07-16 11:49:33 +0530

    fix: autogenerated graphql.ts
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -50,12 +49,12 @@ export abstract class IMutation {
 }
 
 export class Result {
-  total number;
+  total: number
 }
 
 export abstract class ISubscription {
-  abstract totalUpdated(): Nullable<Result> | Promise<Nullable<Result>>;
+  abstract totalUpdated(): Nullable<Result> | Promise<Nullable<Result>>
 }
 
-export type DateTime = any;
-type Nullable<T> = T | null;
+export type DateTime = any
+type Nullable<T> = T | null
