a8e7be79a8d28a947ebb6e62bc7068d6bc0dfedc
Hiro <laciferin@gmail.com>
2023-07-16 11:47:40 +0530

    graphql: add publishers, subscriptions
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 3 minutes

--- src/donations/donations.resolver.ts
+++ src/donations/donations.resolver.ts
@@ -1,15 +1,25 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql'
 import { DonationsService } from './donations.service'
 
 import { Prisma } from '@prisma/client'
+import { PubSub } from 'graphql-subscriptions'
 
+const pubSub = new PubSub()
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
 
   @Mutation('createDonation')
-  create(@Args('createDonationInput') createDonationInput: Prisma.DonationCreateInput) {
-    return this.donationsService.create(createDonationInput)
+  async create(
+    @Args('createDonationInput') createDonationInput: Prisma.DonationCreateInput,
+  ) {
+    const created = await this.donationsService.create(createDonationInput)
+
+    const total = await this.donationsService.getTotal()
+
+    pubSub.publish('totalUpdated', { totalUpdated: { total } })
+
+    return created
   }
 
   @Query('donations')
@@ -17,6 +27,11 @@ export class DonationsResolver {
     //FIXME: OrderByParams
     console.debug({ findAll: orderBy })
     return this.donationsService.findAll(orderBy)
+  }
+
+  @Subscription()
+  totalUpdated() {
+    return pubSub.asyncIterator('totalUpdated')
   }
 
   @Query('donation')
