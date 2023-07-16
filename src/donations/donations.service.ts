f267b114d583d0b4ec24f87108c4dbd80822e432
Hiro <laciferin@gmail.com>
2023-07-16 05:34:00 +0530

    inject: prisma service
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 5 minutes

--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { CreateDonationInput } from './dto/create-donation.input'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class DonationsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createDonationInput: CreateDonationInput) {
     return 'This action adds a new donation'
   }
